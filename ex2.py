7861a25890b5de52aa905469e3737845888777ad
NathaliaMartins <nathaliafloresmartins4550@gmail.com>
2024-10-20 22:10:32 -0300

    Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações

--- ex2.py
+++ ex2.py
@@ -1,4 +1,3 @@
-'''Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações'''
 user = input("Insira seu nome ")
 pass_word = input("Insira a senha ")
 
@@ -7,4 +6,4 @@ while (user == pass_word):
     user = input("Insira seu nome ")
     pass_word = input("Insira a senha ")
 
-print("Acesso Autorizado")
+print("Acesso autorizado")
